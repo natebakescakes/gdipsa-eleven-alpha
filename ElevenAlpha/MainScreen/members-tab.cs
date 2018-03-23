93a85a0f0258ee9414c5596cb4a5e1043bc24132
Raniah Abdul Razak <raniarazzak@gmail.com>
2018-03-23 14:09:31 +0800

    Created a custom control for members tab

---
+++ ElevenAlpha/MainScreen/members-tab.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MainScreen
+{
+    public partial class members_tab : UserControl
+    {
+        public members_tab()
+        {
+            InitializeComponent();
+        }
+    }
+}

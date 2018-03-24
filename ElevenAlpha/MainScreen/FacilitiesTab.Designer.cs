561ee0e7c7c4014c013babc71502dfb62e5cf721
jinkanggg <e0282985@u.nus.edu>
2018-03-24 17:07:23 +0800

    facilities tab done

--- ElevenAlpha/MainScreen/FacilitiesTab.Designer.cs
+++ ElevenAlpha/MainScreen/FacilitiesTab.Designer.cs
@@ -28,56 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.FacilitiesdataGridView1 = new System.Windows.Forms.DataGridView();
             this.FacilititesTabBtnNew = new System.Windows.Forms.Button();
             this.FacilitiesTabBtnEdit = new System.Windows.Forms.Button();
-            this.FacilitiesTabBtnDelete = new System.Windows.Forms.Button();
+            this.FacilitiesTabBtnDeactivate = new System.Windows.Forms.Button();
             this.FacilitiesTabBtnViewHist = new System.Windows.Forms.Button();
             this.ActiveCheckbox = new System.Windows.Forms.CheckBox();
             this.SearchTextbox = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.FacilitiesTabBtnActivate = new System.Windows.Forms.Button();
+            this.SearchLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.FacilitiesdataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // FacilitiesdataGridView1
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(842, 417);
-            this.dataGridView1.TabIndex = 1;
+            this.FacilitiesdataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.FacilitiesdataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.FacilitiesdataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.FacilitiesdataGridView1.Name = "FacilitiesdataGridView1";
+            this.FacilitiesdataGridView1.RowTemplate.Height = 28;
+            this.FacilitiesdataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.FacilitiesdataGridView1.Size = new System.Drawing.Size(842, 417);
+            this.FacilitiesdataGridView1.TabIndex = 1;
             // 
             // FacilititesTabBtnNew
             // 
-            this.FacilititesTabBtnNew.Location = new System.Drawing.Point(224, 439);
+            this.FacilititesTabBtnNew.Location = new System.Drawing.Point(170, 438);
             this.FacilititesTabBtnNew.Name = "FacilititesTabBtnNew";
-            this.FacilititesTabBtnNew.Size = new System.Drawing.Size(75, 29);
+            this.FacilititesTabBtnNew.Size = new System.Drawing.Size(66, 29);
             this.FacilititesTabBtnNew.TabIndex = 2;
             this.FacilititesTabBtnNew.Text = "New";
             this.FacilititesTabBtnNew.UseVisualStyleBackColor = true;
             // 
             // FacilitiesTabBtnEdit
             // 
-            this.FacilitiesTabBtnEdit.Location = new System.Drawing.Point(305, 439);
+            this.FacilitiesTabBtnEdit.Location = new System.Drawing.Point(242, 438);
             this.FacilitiesTabBtnEdit.Name = "FacilitiesTabBtnEdit";
-            this.FacilitiesTabBtnEdit.Size = new System.Drawing.Size(75, 29);
+            this.FacilitiesTabBtnEdit.Size = new System.Drawing.Size(66, 29);
             this.FacilitiesTabBtnEdit.TabIndex = 3;
             this.FacilitiesTabBtnEdit.Text = "Edit";
             this.FacilitiesTabBtnEdit.UseVisualStyleBackColor = true;
             // 
-            // FacilitiesTabBtnDelete
+            // FacilitiesTabBtnDeactivate
             // 
-            this.FacilitiesTabBtnDelete.Location = new System.Drawing.Point(386, 439);
-            this.FacilitiesTabBtnDelete.Name = "FacilitiesTabBtnDelete";
-            this.FacilitiesTabBtnDelete.Size = new System.Drawing.Size(75, 29);
-            this.FacilitiesTabBtnDelete.TabIndex = 4;
-            this.FacilitiesTabBtnDelete.Text = "Delete";
-            this.FacilitiesTabBtnDelete.UseVisualStyleBackColor = true;
+            this.FacilitiesTabBtnDeactivate.Location = new System.Drawing.Point(314, 438);
+            this.FacilitiesTabBtnDeactivate.Name = "FacilitiesTabBtnDeactivate";
+            this.FacilitiesTabBtnDeactivate.Size = new System.Drawing.Size(98, 29);
+            this.FacilitiesTabBtnDeactivate.TabIndex = 4;
+            this.FacilitiesTabBtnDeactivate.Text = "Deactivate";
+            this.FacilitiesTabBtnDeactivate.UseVisualStyleBackColor = true;
+            this.FacilitiesTabBtnDeactivate.Click += new System.EventHandler(this.FacilitiesTabBtnDeactivate_Click);
             // 
             // FacilitiesTabBtnViewHist
             // 
-            this.FacilitiesTabBtnViewHist.Location = new System.Drawing.Point(467, 439);
+            this.FacilitiesTabBtnViewHist.Location = new System.Drawing.Point(512, 438);
             this.FacilitiesTabBtnViewHist.Name = "FacilitiesTabBtnViewHist";
             this.FacilitiesTabBtnViewHist.Size = new System.Drawing.Size(178, 29);
             this.FacilitiesTabBtnViewHist.TabIndex = 5;
@@ -87,51 +91,75 @@
             // ActiveCheckbox
             // 
             this.ActiveCheckbox.AutoSize = true;
-            this.ActiveCheckbox.Location = new System.Drawing.Point(678, 439);
+            this.ActiveCheckbox.Location = new System.Drawing.Point(705, 441);
             this.ActiveCheckbox.Name = "ActiveCheckbox";
             this.ActiveCheckbox.Size = new System.Drawing.Size(134, 24);
             this.ActiveCheckbox.TabIndex = 6;
             this.ActiveCheckbox.Text = "Show Inactive";
             this.ActiveCheckbox.UseVisualStyleBackColor = true;
+            this.ActiveCheckbox.CheckedChanged += new System.EventHandler(this.ActiveCheckbox_CheckedChanged);
+            this.ActiveCheckbox.Click += new System.EventHandler(this.SearchTextbox_Enter);
             // 
             // SearchTextbox
             // 
             this.SearchTextbox.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
-            this.SearchTextbox.Location = new System.Drawing.Point(16, 439);
+            this.SearchTextbox.Location = new System.Drawing.Point(14, 439);
             this.SearchTextbox.Name = "SearchTextbox";
-            this.SearchTextbox.Size = new System.Drawing.Size(184, 26);
+            this.SearchTextbox.Size = new System.Drawing.Size(135, 26);
             this.SearchTextbox.TabIndex = 7;
-            this.SearchTextbox.Text = "Search...";
-            this.SearchTextbox.Enter += new System.EventHandler(this.SearchTextbox_Enter);
-            this.SearchTextbox.Leave += new System.EventHandler(this.SearchTextbox_Leave);
+            this.SearchTextbox.TextChanged += new System.EventHandler(this.SearchTextbox_TextChanged);
+            // 
+            // FacilitiesTabBtnActivate
+            // 
+            this.FacilitiesTabBtnActivate.Location = new System.Drawing.Point(418, 438);
+            this.FacilitiesTabBtnActivate.Name = "FacilitiesTabBtnActivate";
+            this.FacilitiesTabBtnActivate.Size = new System.Drawing.Size(75, 29);
+            this.FacilitiesTabBtnActivate.TabIndex = 8;
+            this.FacilitiesTabBtnActivate.Text = "Activate";
+            this.FacilitiesTabBtnActivate.UseVisualStyleBackColor = true;
+            this.FacilitiesTabBtnActivate.Click += new System.EventHandler(this.FacilitiesTabBtnActivate_Click);
+            // 
+            // SearchLabel
+            // 
+            this.SearchLabel.AutoSize = true;
+            this.SearchLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SearchLabel.Location = new System.Drawing.Point(21, 420);
+            this.SearchLabel.Name = "SearchLabel";
+            this.SearchLabel.Size = new System.Drawing.Size(53, 17);
+            this.SearchLabel.TabIndex = 9;
+            this.SearchLabel.Text = "Search";
             // 
             // FacilitiesTab
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.SearchLabel);
+            this.Controls.Add(this.FacilitiesTabBtnActivate);
             this.Controls.Add(this.SearchTextbox);
             this.Controls.Add(this.ActiveCheckbox);
             this.Controls.Add(this.FacilitiesTabBtnViewHist);
-            this.Controls.Add(this.FacilitiesTabBtnDelete);
+            this.Controls.Add(this.FacilitiesTabBtnDeactivate);
             this.Controls.Add(this.FacilitiesTabBtnEdit);
             this.Controls.Add(this.FacilititesTabBtnNew);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.FacilitiesdataGridView1);
             this.Name = "FacilitiesTab";
             this.Padding = new System.Windows.Forms.Padding(0, 0, 0, 75);
             this.Size = new System.Drawing.Size(842, 492);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FacilitiesdataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView FacilitiesdataGridView1;
         private System.Windows.Forms.Button FacilititesTabBtnNew;
         private System.Windows.Forms.Button FacilitiesTabBtnEdit;
-        private System.Windows.Forms.Button FacilitiesTabBtnDelete;
+        private System.Windows.Forms.Button FacilitiesTabBtnDeactivate;
         private System.Windows.Forms.Button FacilitiesTabBtnViewHist;
         private System.Windows.Forms.CheckBox ActiveCheckbox;
         private System.Windows.Forms.TextBox SearchTextbox;
+        private System.Windows.Forms.Button FacilitiesTabBtnActivate;
+        private System.Windows.Forms.Label SearchLabel;
     }
 }
--- ElevenAlpha/MainScreen/FacilitiesTab.cs
+++ ElevenAlpha/MainScreen/FacilitiesTab.cs
@@ -17,71 +17,195 @@ namespace ElevenAlpha
         {
             InitializeComponent();
             //Data Grid Columns
-            dataGridView1.ColumnCount = 6;
-            for (int i = 0; i<dataGridView1.ColumnCount;i++)
+            FacilitiesdataGridView1.ColumnCount = 6;
+            for (int i = 0; i < FacilitiesdataGridView1.ColumnCount; i++)
             {
-                string[] FacilityGridColumns = new string[] { "Facility ID", "Facility Name","Facility Type", "Opening Hours", "No. of Slots", "Active" };
-                dataGridView1.Columns[i].Name = FacilityGridColumns[i];
-                dataGridView1.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                string[] FacilityGridColumns = new string[] { "Facility ID", "Facility Name", "Facility Type", "Opening Hours", "No. of Slots", "Active" };
+                FacilitiesdataGridView1.Columns[i].Name = FacilityGridColumns[i];
+                FacilitiesdataGridView1.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
             }
-            //filling columns
-            dataGridView1.AutoGenerateColumns = false;
-            ElevenAlphaEntities context = new ElevenAlphaEntities();
-            var columns = context.Facilities.Select
-                (x => new Result{ FacilityID= x.FacilityID, Name = x.Name,
-                TypeID = (int)x.TypeID, Active = (int)x.Active,
-                OpeningTime = (DateTime)x.OpeningTime, ClosingTime = (DateTime)x.ClosingTime
-                
 
-             });
+            LoadDataGridView();
+
+        }
+
+        private void LoadDataGridView()
+        {
+            
+            //filling columns
+            FacilitiesdataGridView1.AutoGenerateColumns = false;
+            ElevenAlphaEntities context = new ElevenAlphaEntities();
+            var columns = context.Facilities
+                .Select
+                (x => new Result
+                {
+                    FacilityID = x.FacilityID,
+                    Name = x.Name,
+                    TypeID = (int)x.TypeID,
+                    Active = (int)x.Active,
+                    OpeningTime = (DateTime)x.OpeningTime,
+                    ClosingTime = (DateTime)x.ClosingTime
+
+                });
 
             var resultsList = columns.ToList();
-            for (int i=0; i< resultsList.Count; i++)
+            for (int i = 0; i < resultsList.Count; i++)
             {
+                //Opening time to closing time
                 var result = resultsList[i];
                 var opening = (result.OpeningTime).ToString("H:mm");
                 var closing = (result.ClosingTime).ToString("H:mm");
                 result.Difference = opening + " - " + closing;
 
+                //number of hours between opening and closing
                 var start = (result.OpeningTime.TimeOfDay);
                 var end = (result.ClosingTime.TimeOfDay);
                 TimeSpan duration = end - start;
                 result.HoursOpen = int.Parse(duration.TotalHours.ToString());
-               
+
+                //active status
+                var activestatus = (result.Active.ToString());
+                if (activestatus == "1")
+                {
+                    activestatus = "Active";
+                }
+                else
+                {
+                    activestatus = "Deactivated";
+                }
+                result.ActiveStatus = activestatus;
             }
 
-            dataGridView1.Columns[0].DataPropertyName = "FacilityID";
-            dataGridView1.Columns[1].DataPropertyName = "Name";
-            dataGridView1.Columns[2].DataPropertyName = "TypeID";
-            dataGridView1.Columns[3].DataPropertyName = "Difference";
-            dataGridView1.Columns[4].DataPropertyName = "HoursOpen";
-            dataGridView1.Columns[5].DataPropertyName = "Active";
-            dataGridView1.DataSource = resultsList;
-
-
+            FacilitiesdataGridView1.Columns[0].DataPropertyName = "FacilityID";
+            FacilitiesdataGridView1.Columns[1].DataPropertyName = "Name";
+            FacilitiesdataGridView1.Columns[2].DataPropertyName = "TypeID";
+            FacilitiesdataGridView1.Columns[3].DataPropertyName = "Difference";
+            FacilitiesdataGridView1.Columns[4].DataPropertyName = "HoursOpen";
+            FacilitiesdataGridView1.Columns[5].DataPropertyName = "ActiveStatus";
+            FacilitiesdataGridView1.DataSource = resultsList;
         }
-        private string getDifference(DateTime start, DateTime end)
+
+        private void SearchTextbox_TextChanged(object sender, EventArgs e)
         {
-            return "hi";
-            //return start.ToString().Substring(start.ToString().IndexOf("1900") + 6) + " to " + end.ToString().Substring(end.ToString().IndexOf("1900") + 6);
-        }
 
+            ElevenAlphaEntities context = new ElevenAlphaEntities();
+            var searchValue = SearchTextbox.Text.Trim();
+            var columns = context.Facilities
+                .Where(x => x.Name.Contains(searchValue) || x.FacilityID.ToString().Contains(searchValue))
+                .Select(x => new Result
+                {
+                    FacilityID = x.FacilityID,
+                    Name = x.Name,
+                    TypeID = (int)x.TypeID,
+                    Active = (int)x.Active,
+                    OpeningTime = (DateTime)x.OpeningTime,
+                    ClosingTime = (DateTime)x.ClosingTime
+
+                });
+
+            var resultsList = columns.ToList();
+            for (int i = 0; i < resultsList.Count; i++)
+            {
+                //Opening time to closing time
+                var result = resultsList[i];
+                var opening = (result.OpeningTime).ToString("H:mm");
+                var closing = (result.ClosingTime).ToString("H:mm");
+                result.Difference = opening + " - " + closing;
+
+                //number of hours between opening and closing
+                var start = (result.OpeningTime.TimeOfDay);
+                var end = (result.ClosingTime.TimeOfDay);
+                TimeSpan duration = end - start;
+                result.HoursOpen = int.Parse(duration.TotalHours.ToString());
+
+                //active status
+                var activestatus = (result.Active.ToString());
+                if (activestatus == "1")
+                {
+                    activestatus = "Active";
+                }
+                else
+                {
+                    activestatus = "Deactivated";
+                }
+                result.ActiveStatus = activestatus;
+            }
+
+            FacilitiesdataGridView1.Columns[0].DataPropertyName = "FacilityID";
+            FacilitiesdataGridView1.Columns[1].DataPropertyName = "Name";
+            FacilitiesdataGridView1.Columns[2].DataPropertyName = "TypeID";
+            FacilitiesdataGridView1.Columns[3].DataPropertyName = "Difference";
+            FacilitiesdataGridView1.Columns[4].DataPropertyName = "HoursOpen";
+            FacilitiesdataGridView1.Columns[5].DataPropertyName = "ActiveStatus";
+            FacilitiesdataGridView1.DataSource = resultsList;
+            
+        }
+        //Cue for searchbox
         private void SearchTextbox_Enter(object sender, EventArgs e)
         {
-            if (SearchTextbox.Text == "Search...")
-            {
-                SearchTextbox.Text = "";
-                SearchTextbox.ForeColor = Color.Black;
-            }
+            
         }
-        private void SearchTextbox_Leave(object sender, EventArgs e)
+        
+        //Hiding or showing deactivated facilities via checkbox
+        private void ActiveCheckbox_CheckedChanged(object sender, EventArgs e)
         {
-            if (SearchTextbox.Text == "")
+            CurrencyManager currencyManager1 = (CurrencyManager)BindingContext[FacilitiesdataGridView1.DataSource];
+            currencyManager1.SuspendBinding();
+            for (int i = 0; i < FacilitiesdataGridView1.RowCount; i++)
             {
-                SearchTextbox.Text = "Search...";
-                SearchTextbox.ForeColor = Color.Silver;
+                if (FacilitiesdataGridView1.Rows[i].Cells[5].Value.ToString() == "Deactivated" && ActiveCheckbox.Checked)
+                {
+                    FacilitiesdataGridView1.Rows[i].Visible = false;
+                }   
+                if (FacilitiesdataGridView1.Rows[i].Cells[5].Value.ToString() == "Deactivated" && !ActiveCheckbox.Checked)
+                {
+                    FacilitiesdataGridView1.Rows[i].Visible = true;
+                }
             }
         }
+        //Activating facilities
+        private void FacilitiesTabBtnActivate_Click(object sender, EventArgs e)
+        {
+            ElevenAlphaEntities context = new ElevenAlphaEntities();
+            int q = (int)FacilitiesdataGridView1.SelectedRows[0].Cells[0].Value;
+            Facility toactive = context.Facilities.Where(x => x.FacilityID == q).First();
+
+            if (toactive.Active == 1)
+            {
+                MessageBox.Show("Facility already activated");
+            }
+            else 
+            {
+                toactive.Active = 1;
+                MessageBox.Show("Facility activated");
+                context.SaveChanges();
+                LoadDataGridView();
+            }
+            
+        }
+        //deactivating facilities
+        private void FacilitiesTabBtnDeactivate_Click(object sender, EventArgs e)//already deactivated keeps popping out
+        {
+            ElevenAlphaEntities context = new ElevenAlphaEntities();
+            int q = (int)FacilitiesdataGridView1.SelectedRows[0].Cells[0].Value;
+            Facility toinactive = context.Facilities.Where(x => x.FacilityID == q).First();
+
+
+
+            if (toinactive.Active == 0)
+            {
+                MessageBox.Show("Facility already deactivated");
+            }
+            else
+            {
+                toinactive.Active = 0;
+                MessageBox.Show("Facility deactivated");
+                context.SaveChanges();
+                LoadDataGridView();
+            }
+        }
+
+        
     }
     public class Result
     {
@@ -93,6 +217,7 @@ namespace ElevenAlpha
         public DateTime ClosingTime { get; set; }
         public string Difference { get; set; }
         public double HoursOpen { get; set; }
+        public string ActiveStatus { get; set; }
          
     }
 }

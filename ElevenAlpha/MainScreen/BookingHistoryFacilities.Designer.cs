ddebf63ec74da6d59d48a5baa343616e499a60eb
natebakescakes <natebakescakes@gmail.com>
2018-03-24 15:31:23 +0800

    Initial Layout

---
+++ ElevenAlpha/MainScreen/BookingHistoryFacilities.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace ElevenAlpha
+{
+    partial class BookingHistoryFacilities
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.BookingMemberDataGrid = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.TildeLabel = new System.Windows.Forms.Label();
+            this.MemberIdTextBox = new System.Windows.Forms.TextBox();
+            this.MemberIdLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.ShowCompletedCheckBox = new System.Windows.Forms.CheckBox();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.CancelBookingButton = new System.Windows.Forms.Button();
+            this.PrintReceiptButton = new System.Windows.Forms.Button();
+            this.ViewReceiptButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BookingMemberDataGrid)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.BookingMemberDataGrid, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 4);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(100, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 56F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 75F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1296, 706);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // BookingMemberDataGrid
+            // 
+            this.BookingMemberDataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.BookingMemberDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.BookingMemberDataGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BookingMemberDataGrid.Location = new System.Drawing.Point(3, 79);
+            this.BookingMemberDataGrid.Name = "BookingMemberDataGrid";
+            this.BookingMemberDataGrid.RowTemplate.Height = 40;
+            this.BookingMemberDataGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.BookingMemberDataGrid.Size = new System.Drawing.Size(1290, 529);
+            this.BookingMemberDataGrid.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 8;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 112F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 165F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 180F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 181F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 34F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 180F));
+            this.tableLayoutPanel2.Controls.Add(this.dateTimePicker1, 7, 0);
+            this.tableLayoutPanel2.Controls.Add(this.dateTimePicker2, 5, 0);
+            this.tableLayoutPanel2.Controls.Add(this.TildeLabel, 6, 0);
+            this.tableLayoutPanel2.Controls.Add(this.MemberIdTextBox, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.MemberIdLabel, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.textBox2, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 23);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1290, 50);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(1113, 3);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(174, 38);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker2.Location = new System.Drawing.Point(898, 3);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(175, 38);
+            this.dateTimePicker2.TabIndex = 1;
+            // 
+            // TildeLabel
+            // 
+            this.TildeLabel.AutoSize = true;
+            this.TildeLabel.Location = new System.Drawing.Point(1079, 0);
+            this.TildeLabel.Name = "TildeLabel";
+            this.TildeLabel.Padding = new System.Windows.Forms.Padding(0, 10, 0, 0);
+            this.TildeLabel.Size = new System.Drawing.Size(28, 42);
+            this.TildeLabel.TabIndex = 2;
+            this.TildeLabel.Text = "~";
+            // 
+            // MemberIdTextBox
+            // 
+            this.MemberIdTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MemberIdTextBox.Location = new System.Drawing.Point(698, 3);
+            this.MemberIdTextBox.Name = "MemberIdTextBox";
+            this.MemberIdTextBox.Size = new System.Drawing.Size(174, 38);
+            this.MemberIdTextBox.TabIndex = 3;
+            // 
+            // MemberIdLabel
+            // 
+            this.MemberIdLabel.AutoSize = true;
+            this.MemberIdLabel.Location = new System.Drawing.Point(533, 0);
+            this.MemberIdLabel.Name = "MemberIdLabel";
+            this.MemberIdLabel.Padding = new System.Windows.Forms.Padding(0, 5, 0, 0);
+            this.MemberIdLabel.Size = new System.Drawing.Size(139, 37);
+            this.MemberIdLabel.TabIndex = 4;
+            this.MemberIdLabel.Text = "Facility ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(0, 5, 0, 0);
+            this.label2.Size = new System.Drawing.Size(105, 37);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Search";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox2.Location = new System.Drawing.Point(115, 3);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(412, 38);
+            this.textBox2.TabIndex = 6;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 7;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 55F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 211F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 219F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 241F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 153F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 45F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel3.Controls.Add(this.ShowCompletedCheckBox, 6, 0);
+            this.tableLayoutPanel3.Controls.Add(this.CloseButton, 4, 0);
+            this.tableLayoutPanel3.Controls.Add(this.CancelBookingButton, 3, 0);
+            this.tableLayoutPanel3.Controls.Add(this.PrintReceiptButton, 2, 0);
+            this.tableLayoutPanel3.Controls.Add(this.ViewReceiptButton, 1, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 634);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(1290, 69);
+            this.tableLayoutPanel3.TabIndex = 2;
+            // 
+            // ShowCompletedCheckBox
+            // 
+            this.ShowCompletedCheckBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ShowCompletedCheckBox.AutoSize = true;
+            this.ShowCompletedCheckBox.Location = new System.Drawing.Point(1018, 3);
+            this.ShowCompletedCheckBox.Name = "ShowCompletedCheckBox";
+            this.ShowCompletedCheckBox.Padding = new System.Windows.Forms.Padding(0, 5, 0, 0);
+            this.ShowCompletedCheckBox.Size = new System.Drawing.Size(269, 41);
+            this.ShowCompletedCheckBox.TabIndex = 0;
+            this.ShowCompletedCheckBox.Text = "Show Completed";
+            this.ShowCompletedCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.CloseButton.Location = new System.Drawing.Point(779, 3);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(147, 43);
+            this.CloseButton.TabIndex = 1;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            // 
+            // CancelBookingButton
+            // 
+            this.CancelBookingButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.CancelBookingButton.Location = new System.Drawing.Point(538, 3);
+            this.CancelBookingButton.Name = "CancelBookingButton";
+            this.CancelBookingButton.Size = new System.Drawing.Size(235, 43);
+            this.CancelBookingButton.TabIndex = 2;
+            this.CancelBookingButton.Text = "Cancel Booking";
+            this.CancelBookingButton.UseVisualStyleBackColor = true;
+            // 
+            // PrintReceiptButton
+            // 
+            this.PrintReceiptButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PrintReceiptButton.Location = new System.Drawing.Point(319, 3);
+            this.PrintReceiptButton.Name = "PrintReceiptButton";
+            this.PrintReceiptButton.Size = new System.Drawing.Size(213, 43);
+            this.PrintReceiptButton.TabIndex = 3;
+            this.PrintReceiptButton.Text = "Print Receipt";
+            this.PrintReceiptButton.UseVisualStyleBackColor = true;
+            // 
+            // ViewReceiptButton
+            // 
+            this.ViewReceiptButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ViewReceiptButton.Location = new System.Drawing.Point(108, 3);
+            this.ViewReceiptButton.Name = "ViewReceiptButton";
+            this.ViewReceiptButton.Size = new System.Drawing.Size(205, 43);
+            this.ViewReceiptButton.TabIndex = 4;
+            this.ViewReceiptButton.Text = "View Receipt";
+            this.ViewReceiptButton.UseVisualStyleBackColor = true;
+            // 
+            // BookingHistoryFacilities
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1496, 706);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "BookingHistoryFacilities";
+            this.Padding = new System.Windows.Forms.Padding(100, 0, 100, 0);
+            this.Text = "Booking History (Facilities)";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.BookingMemberDataGrid)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView BookingMemberDataGrid;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label TildeLabel;
+        private System.Windows.Forms.TextBox MemberIdTextBox;
+        private System.Windows.Forms.Label MemberIdLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.CheckBox ShowCompletedCheckBox;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Button CancelBookingButton;
+        private System.Windows.Forms.Button PrintReceiptButton;
+        private System.Windows.Forms.Button ViewReceiptButton;
+    }
+}

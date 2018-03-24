c29e568ad3864002b171a23081bf7f21f182d81d
Raniah Abdul Razak <raniarazzak@gmail.com>
2018-03-24 15:02:47 +0800

    Added custom column headers, activate member button.

--- ElevenAlpha/MainScreen/members-tab.Designer.cs
+++ ElevenAlpha/MainScreen/members-tab.Designer.cs
@@ -36,6 +36,7 @@
             this.SearchTextBox = new System.Windows.Forms.TextBox();
             this.InactiveCheckbox = new System.Windows.Forms.CheckBox();
             this.SearchLabel = new System.Windows.Forms.Label();
+            this.ActivateMemberButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.MemberInfoTable)).BeginInit();
             this.SuspendLayout();
             // 
@@ -47,20 +48,18 @@
             this.MemberInfoTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.MemberInfoTable.Dock = System.Windows.Forms.DockStyle.Fill;
             this.MemberInfoTable.Location = new System.Drawing.Point(0, 0);
-            this.MemberInfoTable.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.MemberInfoTable.Name = "MemberInfoTable";
             this.MemberInfoTable.RowTemplate.Height = 28;
             this.MemberInfoTable.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.MemberInfoTable.Size = new System.Drawing.Size(1834, 682);
+            this.MemberInfoTable.Size = new System.Drawing.Size(1032, 440);
             this.MemberInfoTable.TabIndex = 0;
             // 
             // NewButton
             // 
             this.NewButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.NewButton.Location = new System.Drawing.Point(493, 735);
-            this.NewButton.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.NewButton.Location = new System.Drawing.Point(277, 474);
             this.NewButton.Name = "NewButton";
-            this.NewButton.Size = new System.Drawing.Size(169, 45);
+            this.NewButton.Size = new System.Drawing.Size(95, 29);
             this.NewButton.TabIndex = 3;
             this.NewButton.Text = "New";
             this.NewButton.UseVisualStyleBackColor = true;
@@ -68,10 +67,9 @@
             // EditButton
             // 
             this.EditButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.EditButton.Location = new System.Drawing.Point(672, 735);
-            this.EditButton.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.EditButton.Location = new System.Drawing.Point(378, 474);
             this.EditButton.Name = "EditButton";
-            this.EditButton.Size = new System.Drawing.Size(169, 45);
+            this.EditButton.Size = new System.Drawing.Size(95, 29);
             this.EditButton.TabIndex = 3;
             this.EditButton.Text = "Edit";
             this.EditButton.UseVisualStyleBackColor = true;
@@ -79,10 +77,9 @@
             // DeleteButton
             // 
             this.DeleteButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.DeleteButton.Location = new System.Drawing.Point(851, 735);
-            this.DeleteButton.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.DeleteButton.Location = new System.Drawing.Point(479, 474);
             this.DeleteButton.Name = "DeleteButton";
-            this.DeleteButton.Size = new System.Drawing.Size(169, 45);
+            this.DeleteButton.Size = new System.Drawing.Size(95, 29);
             this.DeleteButton.TabIndex = 4;
             this.DeleteButton.Text = "Delete";
             this.DeleteButton.UseVisualStyleBackColor = true;
@@ -91,10 +88,9 @@
             // BookHstryBttn
             // 
             this.BookHstryBttn.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.BookHstryBttn.Location = new System.Drawing.Point(1030, 735);
-            this.BookHstryBttn.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.BookHstryBttn.Location = new System.Drawing.Point(579, 474);
             this.BookHstryBttn.Name = "BookHstryBttn";
-            this.BookHstryBttn.Size = new System.Drawing.Size(325, 45);
+            this.BookHstryBttn.Size = new System.Drawing.Size(183, 29);
             this.BookHstryBttn.TabIndex = 4;
             this.BookHstryBttn.Text = "View Booking History";
             this.BookHstryBttn.UseVisualStyleBackColor = true;
@@ -103,10 +99,9 @@
             // SearchTextBox
             // 
             this.SearchTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.SearchTextBox.Location = new System.Drawing.Point(120, 738);
-            this.SearchTextBox.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.SearchTextBox.Location = new System.Drawing.Point(68, 476);
             this.SearchTextBox.Name = "SearchTextBox";
-            this.SearchTextBox.Size = new System.Drawing.Size(561, 38);
+            this.SearchTextBox.Size = new System.Drawing.Size(317, 26);
             this.SearchTextBox.TabIndex = 5;
             this.SearchTextBox.TextChanged += new System.EventHandler(this.SearchTextBox_TextChanged);
             // 
@@ -114,10 +109,9 @@
             // 
             this.InactiveCheckbox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.InactiveCheckbox.AutoSize = true;
-            this.InactiveCheckbox.Location = new System.Drawing.Point(1485, 740);
-            this.InactiveCheckbox.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.InactiveCheckbox.Location = new System.Drawing.Point(831, 476);
             this.InactiveCheckbox.Name = "InactiveCheckbox";
-            this.InactiveCheckbox.Size = new System.Drawing.Size(344, 36);
+            this.InactiveCheckbox.Size = new System.Drawing.Size(198, 24);
             this.InactiveCheckbox.TabIndex = 6;
             this.InactiveCheckbox.Text = "View Inactive Members";
             this.InactiveCheckbox.UseVisualStyleBackColor = true;
@@ -127,17 +121,28 @@
             // 
             this.SearchLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.SearchLabel.AutoSize = true;
-            this.SearchLabel.Location = new System.Drawing.Point(5, 741);
-            this.SearchLabel.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.SearchLabel.Location = new System.Drawing.Point(3, 478);
             this.SearchLabel.Name = "SearchLabel";
-            this.SearchLabel.Size = new System.Drawing.Size(105, 32);
+            this.SearchLabel.Size = new System.Drawing.Size(60, 20);
             this.SearchLabel.TabIndex = 8;
             this.SearchLabel.Text = "Search";
             // 
+            // ActivateMemberButton
+            // 
+            this.ActivateMemberButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.ActivateMemberButton.Location = new System.Drawing.Point(768, 474);
+            this.ActivateMemberButton.Name = "ActivateMemberButton";
+            this.ActivateMemberButton.Size = new System.Drawing.Size(157, 29);
+            this.ActivateMemberButton.TabIndex = 9;
+            this.ActivateMemberButton.Text = "Activate Member";
+            this.ActivateMemberButton.UseVisualStyleBackColor = true;
+            this.ActivateMemberButton.Click += new System.EventHandler(this.ActivateMemberButton_Click);
+            // 
             // members_tab
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.ActivateMemberButton);
             this.Controls.Add(this.SearchLabel);
             this.Controls.Add(this.InactiveCheckbox);
             this.Controls.Add(this.SearchTextBox);
@@ -146,10 +151,9 @@
             this.Controls.Add(this.EditButton);
             this.Controls.Add(this.NewButton);
             this.Controls.Add(this.MemberInfoTable);
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "members_tab";
-            this.Padding = new System.Windows.Forms.Padding(0, 0, 0, 150);
-            this.Size = new System.Drawing.Size(1834, 832);
+            this.Padding = new System.Windows.Forms.Padding(0, 0, 0, 97);
+            this.Size = new System.Drawing.Size(1032, 537);
             this.Load += new System.EventHandler(this.members_tab_Load);
             ((System.ComponentModel.ISupportInitialize)(this.MemberInfoTable)).EndInit();
             this.ResumeLayout(false);
@@ -167,5 +171,6 @@
         private System.Windows.Forms.TextBox SearchTextBox;
         private System.Windows.Forms.CheckBox InactiveCheckbox;
         private System.Windows.Forms.Label SearchLabel;
+        private System.Windows.Forms.Button ActivateMemberButton;
     }
 }
--- ElevenAlpha/MainScreen/members-tab.cs
+++ ElevenAlpha/MainScreen/members-tab.cs
@@ -28,23 +28,41 @@ namespace ElevenAlpha
         {
             MemberInfoTable.DataSource = ctx.Members
               .Select(x => new { x.MemberID, x.FirstName, x.LastName, x.Gender, x.Mobile, x.Email, x.EmergencyContact, Active = x.Active == 1 ? "Active" : "Discontinued" }).ToList();
+            SetColumnHeader();
         }
 
         private void ViewActiveMembers()
         {
             MemberInfoTable.DataSource = ctx.Members.Where(x => x.Active == 1)
               .Select(x => new { x.MemberID, x.FirstName, x.LastName, x.Gender, x.Mobile, x.Email, x.EmergencyContact, Active = x.Active == 1 ? "Active" : "Discontinued" }).ToList();
+            SetColumnHeader();
         }
 
+        private void SetColumnHeader()
+        {
+            MemberInfoTable.Columns[0].HeaderText = "Member ID";
+            MemberInfoTable.Columns[1].HeaderText = "First Name";
+            MemberInfoTable.Columns[2].HeaderText = "Last Name";
+            MemberInfoTable.Columns[3].HeaderText = "Gender";
+            MemberInfoTable.Columns[4].HeaderText = "Contact Number";
+            MemberInfoTable.Columns[5].HeaderText = "Email Address";
+            MemberInfoTable.Columns[6].HeaderText = "Emergency Contact Number";
+            MemberInfoTable.Columns[7].HeaderText = "Member Status";
+        }
 
         private void SearchAllMembers()
         {
             string searchValue = SearchTextBox.Text.ToString();
             int searchValueLength = searchValue.Length;
             var q = from x in ctx.Members
-                    where x.FirstName.Substring(0, searchValueLength) == searchValue || x.LastName.Substring(0, searchValueLength) == searchValue || x.Mobile.Substring(4, searchValueLength) == searchValue || x.MemberID.ToString().Substring(0, searchValueLength) == searchValue
+                    where x.FirstName.Substring(0, searchValueLength) == searchValue ||
+                    x.LastName.Substring(0, searchValueLength) == searchValue ||
+                    x.Mobile.Substring(4, searchValueLength) == searchValue ||
+                    x.MemberID.ToString().Substring(0, searchValueLength) == searchValue||
+                    x.Email.Substring(0, searchValueLength) == searchValue
                     select new { x.MemberID, x.FirstName, x.LastName, x.Gender, x.Mobile, x.Email, x.EmergencyContact, Active = x.Active == 1 ? "Active" : "Discontinued" };
             MemberInfoTable.DataSource = q.ToList();
+            SetColumnHeader();
         }
 
         private void SearchActiveMembers()
@@ -52,9 +70,15 @@ namespace ElevenAlpha
             string searchValue = SearchTextBox.Text.ToString();
             int searchValueLength = searchValue.Length;
             var q = from x in ctx.Members
-                    where x.Active == 1 && (x.FirstName.Substring(0, searchValueLength) == searchValue || x.LastName.Substring(0, searchValueLength) == searchValue || x.Mobile.Substring(4, searchValueLength) == searchValue || x.MemberID.ToString().Substring(0, searchValueLength) == searchValue)
+                    where x.Active == 1 && 
+                    (x.FirstName.Substring(0, searchValueLength) == searchValue || 
+                     x.LastName.Substring(0, searchValueLength) == searchValue || 
+                     x.Mobile.Substring(4, searchValueLength) == searchValue ||
+                     x.Email.Substring(0, searchValueLength) == searchValue ||
+                     x.MemberID.ToString().Substring(0, searchValueLength) == searchValue)
                     select new { x.MemberID, x.FirstName, x.LastName, x.Gender, x.Mobile, x.Email, x.EmergencyContact, Active = x.Active == 1 ? "Active" : "Discontinued" };
             MemberInfoTable.DataSource = q.ToList();
+            SetColumnHeader();
 
         }
      
@@ -113,17 +137,26 @@ namespace ElevenAlpha
             }
             else
             {
-                memberToDelete.Active = 0;
-                ctx.SaveChanges();
-                MessageBox.Show(String.Format("Member ID: {0}\n Member Name: {1} {2} is discontinued!",memberToDelete.MemberID,memberToDelete.FirstName,memberToDelete.LastName));
+                if (MessageBox.Show("Are you sure you want to discontinue member?", "Confirm Member Deletion", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    memberToDelete.Active = 0;
+                    ctx.SaveChanges();
+    
+                    MessageBox.Show(String.Format("Member ID: {0}\n Member Name: {1} {2} is discontinued!", memberToDelete.MemberID, memberToDelete.FirstName, memberToDelete.LastName));
+                }
+                else
+                {
+                    // user clicked no
+                }
+                
             }
 
             if (InactiveCheckbox.Checked == true)
             {
-                ViewAllMembers();
+                SearchAllMembers();
             }
             else
-                ViewActiveMembers();
+                SearchActiveMembers();
         }
 
         private void BookHstryBttn_Click(object sender, EventArgs e)
@@ -131,6 +164,35 @@ namespace ElevenAlpha
             //int memberID = (int)MemberInfoTable.SelectedRows[0].Cells[0].Value;
             //Member member = (from x in ctx.Members where x.MemberID == memberID select x).First();
             
+        }
+
+        private void ActivateMemberButton_Click(object sender, EventArgs e)
+        {
+            int memberIdToActivate = (int)MemberInfoTable.SelectedRows[0].Cells[0].Value;
+            Member memberToActivate = (from x in ctx.Members where x.MemberID == memberIdToActivate select x).First();
+
+            if (memberToActivate.Active == 1)
+            {
+                MessageBox.Show("Already activated member!");
+            }
+            else
+            {
+                if (MessageBox.Show("Are you sure you want to activate member?", "Confirm Member Activation", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    memberToActivate.Active = 1;
+                    ctx.SaveChanges();
+
+                    MessageBox.Show(String.Format("Member ID: {0}\nMember Name: {1} {2} is activated!", memberToActivate.MemberID, memberToActivate.FirstName, memberToActivate.LastName));
+                }
+               
+            }
+
+            if (InactiveCheckbox.Checked == true)
+            {
+                SearchAllMembers();
+            }
+            else
+                SearchActiveMembers();
         }
     }
 }

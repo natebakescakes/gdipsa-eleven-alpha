e082fa4ba9d6679870c417b95abdca2edae0001e
natebakescakes <natebakescakes@gmail.com>
2018-03-26 20:58:46 +0800

    Complete Occupancy Rates layout

--- ElevenAlpha/MainScreen/Program.cs
+++ ElevenAlpha/MainScreen/Program.cs
@@ -16,11 +16,11 @@ namespace ElevenAlpha
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainScreen());
+            //Application.Run(new MainScreen());
 
             // Debug Application Calls
 
-           // Application.Run(new BookingHistoryMembers(6, new DateTime(2018, 03, 24), new DateTime(2018, 03, 24)));
+            // Application.Run(new BookingHistoryMembers(6, new DateTime(2018, 03, 24), new DateTime(2018, 03, 24)));
             //Application.Run(new BookingHistoryFacilities(17, new DateTime(2018, 03, 24), new DateTime(2018, 03, 24)));
             //Application.Run(new MemberLookup());
             //Application.Run(new BookingsManager("Tennis Court", new DateTime(2018, 03, 24)));
@@ -39,6 +39,8 @@ namespace ElevenAlpha
             //Application.Run(new AddNewMember());
             //Application.Run(new EditMember(68));
             //Application.Run(new ViewBookingReceipt(100));
+
+            Application.Run(new ViewOccupancyRates());
         }
     }
 }

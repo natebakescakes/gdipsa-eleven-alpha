d9eb360155f4269ad7b1fffaeb187043458dbef0
Raniah Abdul Razak <raniarazzak@gmail.com>
2018-03-26 20:42:07 +0800

    Fixed buttons in members tab

--- ElevenAlpha/MainScreen/MembersTab.Designer.cs
+++ ElevenAlpha/MainScreen/MembersTab.Designer.cs
@@ -28,8 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
             this.MemberInfoTable = new System.Windows.Forms.DataGridView();
             this.NewButton = new System.Windows.Forms.Button();
             this.EditButton = new System.Windows.Forms.Button();
@@ -47,30 +46,14 @@
             this.MemberInfoTable.AllowUserToOrderColumns = true;
             this.MemberInfoTable.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.MemberInfoTable.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
-            dataGridViewCellStyle11.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle11.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle11.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle11.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle11.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle11.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle11.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.MemberInfoTable.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle11;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.MemberInfoTable.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.MemberInfoTable.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle7.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle7.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.MemberInfoTable.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
             this.MemberInfoTable.ColumnHeadersHeight = 40;
             this.MemberInfoTable.Dock = System.Windows.Forms.DockStyle.Fill;
             this.MemberInfoTable.Location = new System.Drawing.Point(0, 0);
@@ -79,16 +62,16 @@
             this.MemberInfoTable.Name = "MemberInfoTable";
             this.MemberInfoTable.RowTemplate.Height = 28;
             this.MemberInfoTable.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.MemberInfoTable.Size = new System.Drawing.Size(1849, 613);
+            this.MemberInfoTable.Size = new System.Drawing.Size(1040, 395);
             this.MemberInfoTable.TabIndex = 0;
             // 
             // NewButton
             // 
             this.NewButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.NewButton.Location = new System.Drawing.Point(464, 666);
+            this.NewButton.Location = new System.Drawing.Point(245, 430);
             this.NewButton.Margin = new System.Windows.Forms.Padding(5);
             this.NewButton.Name = "NewButton";
-            this.NewButton.Size = new System.Drawing.Size(117, 45);
+            this.NewButton.Size = new System.Drawing.Size(78, 29);
             this.NewButton.TabIndex = 3;
             this.NewButton.Text = "New";
             this.NewButton.UseVisualStyleBackColor = true;
@@ -97,10 +80,10 @@
             // EditButton
             // 
             this.EditButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.EditButton.Location = new System.Drawing.Point(592, 666);
+            this.EditButton.Location = new System.Drawing.Point(334, 430);
             this.EditButton.Margin = new System.Windows.Forms.Padding(5);
             this.EditButton.Name = "EditButton";
-            this.EditButton.Size = new System.Drawing.Size(117, 45);
+            this.EditButton.Size = new System.Drawing.Size(108, 29);
             this.EditButton.TabIndex = 3;
             this.EditButton.Text = "Edit";
             this.EditButton.UseVisualStyleBackColor = true;
@@ -109,10 +92,10 @@
             // DeleteButton
             // 
             this.DeleteButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.DeleteButton.Location = new System.Drawing.Point(724, 665);
+            this.DeleteButton.Location = new System.Drawing.Point(448, 430);
             this.DeleteButton.Margin = new System.Windows.Forms.Padding(5);
             this.DeleteButton.Name = "DeleteButton";
-            this.DeleteButton.Size = new System.Drawing.Size(95, 29);
+            this.DeleteButton.Size = new System.Drawing.Size(101, 29);
             this.DeleteButton.TabIndex = 4;
             this.DeleteButton.Text = "Deactivate";
             this.DeleteButton.UseVisualStyleBackColor = true;
@@ -121,10 +104,10 @@
             // BookHstryBttn
             // 
             this.BookHstryBttn.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.BookHstryBttn.Location = new System.Drawing.Point(1049, 666);
+            this.BookHstryBttn.Location = new System.Drawing.Point(672, 430);
             this.BookHstryBttn.Margin = new System.Windows.Forms.Padding(5);
             this.BookHstryBttn.Name = "BookHstryBttn";
-            this.BookHstryBttn.Size = new System.Drawing.Size(316, 45);
+            this.BookHstryBttn.Size = new System.Drawing.Size(185, 29);
             this.BookHstryBttn.TabIndex = 4;
             this.BookHstryBttn.Text = "View Booking History";
             this.BookHstryBttn.UseVisualStyleBackColor = true;
@@ -133,7 +116,7 @@
             // SearchTextBox
             // 
             this.SearchTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.SearchTextBox.Location = new System.Drawing.Point(121, 669);
+            this.SearchTextBox.Location = new System.Drawing.Point(69, 430);
             this.SearchTextBox.Margin = new System.Windows.Forms.Padding(5);
             this.SearchTextBox.Name = "SearchTextBox";
             this.SearchTextBox.Size = new System.Drawing.Size(335, 26);
@@ -144,7 +127,7 @@
             // 
             this.HideInactiveCheckbox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.HideInactiveCheckbox.AutoSize = true;
-            this.HideInactiveCheckbox.Location = new System.Drawing.Point(832, 476);
+            this.HideInactiveCheckbox.Location = new System.Drawing.Point(830, 430);
             this.HideInactiveCheckbox.Name = "HideInactiveCheckbox";
             this.HideInactiveCheckbox.Size = new System.Drawing.Size(197, 24);
             this.HideInactiveCheckbox.TabIndex = 6;
@@ -156,7 +139,7 @@
             // 
             this.SearchLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.SearchLabel.AutoSize = true;
-            this.SearchLabel.Location = new System.Drawing.Point(5, 672);
+            this.SearchLabel.Location = new System.Drawing.Point(5, 430);
             this.SearchLabel.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.SearchLabel.Name = "SearchLabel";
             this.SearchLabel.Size = new System.Drawing.Size(60, 20);
@@ -166,10 +149,10 @@
             // ActivateMemberButton
             // 
             this.ActivateMemberButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.ActivateMemberButton.Location = new System.Drawing.Point(905, 666);
+            this.ActivateMemberButton.Location = new System.Drawing.Point(556, 430);
             this.ActivateMemberButton.Margin = new System.Windows.Forms.Padding(5);
             this.ActivateMemberButton.Name = "ActivateMemberButton";
-            this.ActivateMemberButton.Size = new System.Drawing.Size(133, 45);
+            this.ActivateMemberButton.Size = new System.Drawing.Size(109, 29);
             this.ActivateMemberButton.TabIndex = 9;
             this.ActivateMemberButton.Text = "Activate";
             this.ActivateMemberButton.UseVisualStyleBackColor = true;
@@ -190,8 +173,8 @@
             this.Controls.Add(this.MemberInfoTable);
             this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "MembersTab";
-            this.Padding = new System.Windows.Forms.Padding(0, 0, 0, 150);
-            this.Size = new System.Drawing.Size(1849, 763);
+            this.Padding = new System.Windows.Forms.Padding(0, 0, 0, 97);
+            this.Size = new System.Drawing.Size(1040, 492);
             this.Load += new System.EventHandler(this.members_tab_Load);
             ((System.ComponentModel.ISupportInitialize)(this.MemberInfoTable)).EndInit();
             this.ResumeLayout(false);
